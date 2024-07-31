a1200bd2dc173e2616628f061f8df7f4053de58e
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2024-07-31 15:27:53 +0700

    Create sales invoice

---
+++ App/invoice/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import SalesInvoice
+
+class SalesInvoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SalesInvoice
+        fields = [
+            'id',
+            'user',
+            'customer_name',
+            'date',
+            'book_name',
+            'category',
+            'quantity',
+            'price'
+        ]
+        extra_kwargs = {
+            'user': {'read_only': True},
+            'id': {'read_only': True},
+        }
