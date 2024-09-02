ed05cfbd5d566023a63070770d2254134d50f954
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2024-09-02 20:11:22 +0700

    Commit

--- App/book/urls.py
+++ App/book/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('add-book/', views.add_book, name='add_book'),
     path('search-books/', views.search_books, name='search_books'),
+    path('search-bookID/', views.search_bookID, name='search_bookID'),
 ]
