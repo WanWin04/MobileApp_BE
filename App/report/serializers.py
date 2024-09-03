436774829ac3d217542cb48e1f390796d23cc373
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2024-09-03 16:30:41 +0700

    Fix Report

--- App/report/serializers.py
+++ App/report/serializers.py
@@ -4,7 +4,7 @@ from .models import InventoryReport, DebtReport
 class InventoryReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = InventoryReport
-        fields = ['user', 'month', 'book_name', 'begin', 'arise', 'end']
+        fields = ['user', 'month', 'book_id', 'book_name', 'begin', 'arise', 'end']
         extra_kwargs = {
             'user': {'read_only': True},
         }
@@ -13,7 +13,7 @@ class InventoryReportSerializer(serializers.ModelSerializer):
 class DebtReporterSerializer(serializers.ModelSerializer):
     class Meta:
         model = DebtReport
-        fields = ['user' ,'month', 'customer_name', 'begin', 'arise', 'end']
+        fields = ['user' ,'month', 'customer_id', 'customer_name', 'begin', 'arise', 'end']
         extra_kwargs = {
             'user': {'read_only': True},
         }
